4d41ececf8d81adae0dd95f6905da2d2348d9f7e
Vic-Thor <lacroix.victor.pro@gmail.com>
2015-07-09 13:45:40 +0200

    fix: file encoding -> use UTF-8 with BOM fix: replace "    " with tabulation

--- src/legend/Controller.js
+++ src/legend/Controller.js
@@ -4,92 +4,92 @@
  * plugin to promote that instance of this class to the body element.
  */
 Ext.define('ck.legend.Controller', {
-    extend: 'Ext.app.ViewController',
-    alias: 'controller.cklegend',
-    
-    listen: {
-        controller: {
-            'ckmap': {
-                // 
-                ckmapReady: 'onMapReady'
-            }
-        }
-    },
-    
-    
-    init: function() {
-        var olMap = this.getMap();
-        if(!(olMap instanceof ol.Map)) return;
-        
-        this.initTree();
-    },
-    
-    onMapReady: function(ckmap) {
-        this.getView().setMap( ckmap.getMap() );
-        this.init();
-    },
-    
-    getMap: function() {
-        return this.getView().getMap();
-    },
-    
-    initTree: function() {
-        var me = this;
-        var v = me.getView();
-        var root = v.getRootNode();
-        
-        var olMap = me.getMap();
-        var layers = olMap.getLayers();
-        
-        layers.forEach(function(layer){            
-            var node = {
-                leaf: true,
-                text: layer.get('title'),
-                checked: layer.get('visible'),
-                layer: layer
-            };
-            var pNode = root;
-            
-            var path = layer.get('path')
-            if(path) {
-                var keys = path.split('/');
-                var keyId = pKeyId = '';
-                
-                for(i=0; i<keys.length; i++) {
-                    keyId += '_' + keys[i];
-                    var isNode = root.findChild('id', keyId);
-                    if(!isNode) {
-                        pNode = pNode.appendChild({
-                            text: keys[i],
-                            id: keyId,
-                            checked: false
-                        }, true);
-                    } else {
-                        pNode = isNode;
-                    }
-                };
-            }
-            
-            pNode.appendChild(node);
-        });
-        
-        
-        v.getStore().on('update', this.onUpdate);
-        
-        // olMap.on('addlayer', function() {
-            // root.insertBefore(lyr, root); // Pour inserer le layer dans un dossier après
-        // });
-        
-        this.fireEvent('cklegendReady', v);
-    },
-    
-    // bind tree panel to ol map
-    onUpdate: function(store, rec, operation, modifiedFieldNames, details, eOpts) {
-        var layer = rec.get('layer');
-        if(!layer) return;
-        
-        if(modifiedFieldNames=='checked') {
-            layer.set('visible', rec.get('checked'));
-        }
-    }
+	extend: 'Ext.app.ViewController',
+	alias: 'controller.cklegend',
+	
+	listen: {
+		controller: {
+			'ckmap': {
+				// 
+				ckmapReady: 'onMapReady'
+			}
+		}
+	},
+	
+	
+	init: function() {
+		var olMap = this.getMap();
+		if(!(olMap instanceof ol.Map)) return;
+		
+		this.initTree();
+	},
+	
+	onMapReady: function(ckmap) {
+		this.getView().setMap( ckmap.getMap() );
+		this.init();
+	},
+	
+	getMap: function() {
+		return this.getView().getMap();
+	},
+	
+	initTree: function() {
+		var me = this;
+		var v = me.getView();
+		var root = v.getRootNode();
+		
+		var olMap = me.getMap();
+		var layers = olMap.getLayers();
+		
+		layers.forEach(function(layer){			
+			var node = {
+				leaf: true,
+				text: layer.get('title'),
+				checked: layer.get('visible'),
+				layer: layer
+			};
+			var pNode = root;
+			
+			var path = layer.get('path')
+			if(path) {
+				var keys = path.split('/');
+				var keyId = pKeyId = '';
+				
+				for(i=0; i<keys.length; i++) {
+					keyId += '_' + keys[i];
+					var isNode = root.findChild('id', keyId);
+					if(!isNode) {
+						pNode = pNode.appendChild({
+							text: keys[i],
+							id: keyId,
+							checked: false
+						}, true);
+					} else {
+						pNode = isNode;
+					}
+				};
+			}
+			
+			pNode.appendChild(node);
+		});
+		
+		
+		v.getStore().on('update', this.onUpdate);
+		
+		// olMap.on('addlayer', function() {
+			// root.insertBefore(lyr, root); // Pour inserer le layer dans un dossier après
+		// });
+		
+		this.fireEvent('cklegendReady', v);
+	},
+	
+	// bind tree panel to ol map
+	onUpdate: function(store, rec, operation, modifiedFieldNames, details, eOpts) {
+		var layer = rec.get('layer');
+		if(!layer) return;
+		
+		if(modifiedFieldNames=='checked') {
+			layer.set('visible', rec.get('checked'));
+		}
+	}
 });
--- src/legend/plugin/Slider.js
+++ src/legend/plugin/Slider.js
@@ -1,95 +1,95 @@
-/*
+ï»¿/*
  * JML 2014
- * Composant légende à base de Tree.
- * Ajout de cartouche pour la légende, de slider pour la transparence...
+ * Composant lÃ©gende Ã  base de Tree.
+ * Ajout de cartouche pour la lÃ©gende, de slider pour la transparence...
  */
 
 Ext.define('Ext.ux.tree.checker', {
-    extend: 'Ext.AbstractPlugin',
-    alias: 'plugin.ux.checker',
-    
-    init: function(cmp) {
-        cmp.on('checkchange', this.onCheckChange, this);
-    },
-    
-    onCheckChange: function(node, isChecked){
-        // Propagate change downwards (for all children of current node).
-        if (node.hasChildNodes()) {
-            node.eachChild(this.setChildrenCheckedStatus);
-        }        
+	extend: 'Ext.AbstractPlugin',
+	alias: 'plugin.ux.checker',
+	
+	init: function(cmp) {
+		cmp.on('checkchange', this.onCheckChange, this);
+	},
+	
+	onCheckChange: function(node, isChecked){
+		// Propagate change downwards (for all children of current node).
+		if (node.hasChildNodes()) {
+			node.eachChild(this.setChildrenCheckedStatus);
+		}		
 
-        // Propagate change upwards (if all siblings are the same, update parent).
-        this.updateParentCheckedStatus(node);
-    },
-    
-    /* private */
-    setChildrenCheckedStatus: function (current) {
-        if (current.parentNode) {
-            var parent = current.parentNode;
-            if(current.get('checked') !== null){
-                current.set('checked', parent.get('checked'));
-            }
-        }
-        
-        if (current.hasChildNodes()) {
-            current.eachChild(arguments.callee);
-        }
-    },
-    
-    updateParentCheckedStatus: function (current) {
-        if (current.parentNode) {
-            var parent = current.parentNode;
-            
-            var checkedCount = 0;
-            parent.eachChild(function(n) {
-                checkedCount += (n.get('checked') ? 1 : 0);
-            });
-            
-            // Children have same value if all of them are checked or none is checked.
-            // var sameValue = (checkedCount == parent.childNodes.length) || (checkedCount == 0);
+		// Propagate change upwards (if all siblings are the same, update parent).
+		this.updateParentCheckedStatus(node);
+	},
+	
+	/* private */
+	setChildrenCheckedStatus: function (current) {
+		if (current.parentNode) {
+			var parent = current.parentNode;
+			if(current.get('checked') !== null){
+				current.set('checked', parent.get('checked'));
+			}
+		}
+		
+		if (current.hasChildNodes()) {
+			current.eachChild(arguments.callee);
+		}
+	},
+	
+	updateParentCheckedStatus: function (current) {
+		if (current.parentNode) {
+			var parent = current.parentNode;
+			
+			var checkedCount = 0;
+			parent.eachChild(function(n) {
+				checkedCount += (n.get('checked') ? 1 : 0);
+			});
+			
+			// Children have same value if all of them are checked or none is checked.
+			// var sameValue = (checkedCount == parent.childNodes.length) || (checkedCount == 0);
 			 var sameValue = (checkedCount > 0);
-            if(parent.get('checked') !== null){
-                // if (sameValue) {
-                    // var checkedValue = (checkedCount == parent.childNodes.length);
-                    // parent.set('checked', checkedValue);
-                // } else {
-                    // // Not all of the children are checked, so uncheck the parent.
-                    // parent.set('checked', false);
-                // }
+			if(parent.get('checked') !== null){
+				// if (sameValue) {
+					// var checkedValue = (checkedCount == parent.childNodes.length);
+					// parent.set('checked', checkedValue);
+				// } else {
+					// // Not all of the children are checked, so uncheck the parent.
+					// parent.set('checked', false);
+				// }
 				parent.set('checked', sameValue);
-            }
-            this.updateParentCheckedStatus(parent);
-        }
-    }
-    
+			}
+			this.updateParentCheckedStatus(parent);
+		}
+	}
+	
 });
 
 
 
 Ext.define('Ext.ux.tree.slider', {
-    extend: 'Ext.AbstractPlugin',
-    alias: 'plugin.ux.slider',
-    
-    init: function(cmp) {
-        // this.preRenderer = this.renderer || this.defaultRenderer;
-        // this.oScope = this.scope || window;
-        
-        // cmp.on('itemclick', this.onItemclick, this);
-        // cmp.on('itemappend', this.onItemappend, this);
-    }
+	extend: 'Ext.AbstractPlugin',
+	alias: 'plugin.ux.slider',
+	
+	init: function(cmp) {
+		// this.preRenderer = this.renderer || this.defaultRenderer;
+		// this.oScope = this.scope || window;
+		
+		// cmp.on('itemclick', this.onItemclick, this);
+		// cmp.on('itemappend', this.onItemappend, this);
+	}
 
-    /*
-    onItemclick: function(tree, record, item, index, e, eOpts ) {
-        var i = item;
-        
-        this.slider = Ext.create('Ext.slider.Single', {
-            width: 200,
-            value: 50,
-            increment: 10,
-            minValue: 0,
-            maxValue: 100,
-            renderTo: item
-        });       
-    }
-    */
+	/*
+	onItemclick: function(tree, record, item, index, e, eOpts ) {
+		var i = item;
+		
+		this.slider = Ext.create('Ext.slider.Single', {
+			width: 200,
+			value: 50,
+			increment: 10,
+			minValue: 0,
+			maxValue: 100,
+			renderTo: item
+		});	   
+	}
+	*/
 });
