74e2b1e1aeab94f777aed351831f7745a1404752
Vic-Thor <lacroix.victor.pro@gmail.com>
2015-08-24 11:38:47 +0200

    add reset view action

---
+++ src/map/action/ResetView.js
@@ -0,0 +1,32 @@
+/**
+ * Basic action to zoom in the map (zoom level + 1).
+ *
+ * Use on a {@link Ext.button.Button} in a {@link Ext.toolbar.Toolbar}.
+ * 
+ *		{
+ *			xtype: "button",
+ *			scale: "large",
+ *         action: "ckmapZoomin"
+ *		}
+ *
+ * Use on item Menu.
+ *
+ */
+Ext.define('Ck.map.action.ResetView', {
+	extend: 'Ck.Action',
+	alias: "widget.ckmapResetview",
+	
+	itemId: 'resetview',
+	text: '',
+	// iconCls: 'fa fa-globe',
+	iconCls: 'fa fa-home',
+	tooltip: 'Reset view',
+	
+	/**
+	 * Zoom to the initial extent
+	 */
+	doAction: function(btn) {
+		var map = Ck.getMap();
+		map.resetView();
+	}
+});
