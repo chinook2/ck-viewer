8e531e4cd8acbb7042376569191d54d077e2d217
jmargail <julien.margail@gmail.com>
2015-09-18 14:58:49 +0200

    test pull request 2nd ...

--- src/forms/Controller.js
+++ src/forms/Controller.js
@@ -1,5 +1,7 @@
 /**
- * 
+ * The Forms controller allow to load form to display it. It can manage sub-form...
+ *
+ * Also perform load and save data for the form.
  */
 Ext.define('Ck.forms.Controller', {
 	extend: 'Ck.Controller',
