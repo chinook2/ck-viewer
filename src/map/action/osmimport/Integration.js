912ceb0face0760f535b29a6d58907a55ec570f0
Florent RICHARD <florentrichard@hotmail.fr>
2015-11-18 09:10:54 +0100

    Dev: OsmImport: add integration button to the toolbar

---
+++ src/map/action/osmimport/Integration.js
@@ -0,0 +1,53 @@
+/**
+ * Action for OpenStreetMap import.
+ * Opens a panel to set the configuration of the integration and execute it.
+ * @author Florent RICHARD
+ */
+Ext.define('Ck.map.action.osmimport.Integration', {
+	extend: 'Ck.map.action.OsmImport',
+	alias: 'widget.ckmapOsmImportIntegration',
+	
+	itemId: 'osmimportintegration',
+	text: '',
+	iconCls: 'fa fa-save',
+	tooltip: 'Integrate Data',
+	toggleGroup: null,
+	enableToggle: false,
+	
+	doAction: function(btn) {
+		if(!this.win) {
+			this.win = Ext.create('Ext.window.Window', {
+				title: 'OSM Data Integration',
+				// height: 400,
+				width: 600,
+				layout: 'fit',
+				collapsible: true,
+				closable: false,
+				resizable: false,
+				items: [{
+					xtype: "ckosmimportimport",
+					openner: this
+				}]
+				
+			});
+		}
+		this.win.show();
+		this.win.expand();
+	},
+	
+	/**
+	 * Used to close the window.
+	 */
+	close: function() {
+		this.win.hide();
+	},
+	
+	/**
+	 * Collapse the window.
+	 */
+	collapse: function() {
+		this.win.collapse();
+	}
+	
+});
+
