d2e50b430817ba6c57af182e1c0e0cccec554d13
Vic-Thor <lacroix.victor.pro@gmail.com>
2015-12-30 09:08:51 +0100

    fix slider visibility by clicking fix geolocation marker visibility

--- src/map/action/Geolocation.js
+++ src/map/action/Geolocation.js
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  *
  *
  *
@@ -10,58 +10,65 @@
 Ext.define('Ck.map.action.Geolocation', {
 	extend: 'Ck.Action',
 	alias: "widget.ckmapGeolocation",
-	
+
 	toggleGroup: 'geolocation-toggle',
 	tooltip: 'Zoom to your location',
 	iconCls: 'fa fa-street-view',
-	
+
 	itemId: 'geolocation',
-	
+
 	text: '',
-	
+
 	// iconCls: 'fa fa-child',
-	
+
 	// iconCls: 'fa fa-flag',
 	// iconCls: 'fa fa-bullseye',
-	
-	
-	
+
+
+
 	config: {
 		map: null,
-		
+
 		/**
 		 * @var {ol.Geolocation}
 		 */
 		geolocation: null,
-		
+
 		/**
 		 * @var {ol.Overlay}
 		 */
 		geolocationMarker: null
 	},
-	
+
 	/**
 	 * @param {Ck.map.Controller}
 	 */
 	ckLoaded: function(map) {
 		if(!Ext.isEmpty(map.geolocation)) {
 			var body = Ext.getBody();
-			body.insertHtml("BeforeEnd", "<div id='location-gps' class='gps-marker marker-blue'></div>");
-			
+
+			this.marker = body.createChild({
+				"class": "gps-marker marker-blue",
+				"style": {
+					"visibility": "hidden"
+				}
+			});
+
 			this.setGeolocationMarker(new ol.Overlay({
-				element: document.getElementById('location-gps'),
-				positioning: 'bottom-center'
+				element: this.marker.dom,
+				positioning: 'bottom-center',
+				stopEvent: false
 			}));
-			
-			map.geolocation.on('change', function(evt) {             
+
+			map.geolocation.on('change', function(evt) {
 				this.getGeolocationMarker().setPosition(evt.target.getPosition());
 			}, this);
 			this.setGeolocation(map.geolocation);
-			
+
 			map.getOlMap().addOverlay(this.getGeolocationMarker());
 		}
 	},
-	
+
 	/**
 	 * Update geolocationMarker's position via GPS if pressed == true.
 	 * Zoom to user location
@@ -69,16 +76,16 @@ Ext.define('Ck.map.action.Geolocation', {
 	toggleAction: function(btn, pressed) {
 		var olMap = this.getMap().getOlMap();
 		var mark = this.getGeolocationMarker();
-		
+
 		if(pressed) {
-			var p = this.getGeolocation().getPosition();                
-			if(!Ext.isEmpty(p)) {	
-				mark.setVisible(true);
+			var p = this.getGeolocation().getPosition();
+			if(!Ext.isEmpty(p)) {
+				this.marker.setVisible(true);
 				mark.setPosition(p);
 				olMap.getView().setCenter(p);
 			}
 		} else {
-			mark.setVisible(false);
+			this.marker.setVisible(false);
 		}
 	}
 });
