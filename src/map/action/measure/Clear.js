644f1422219f664b20a95861bdaec063eec61168
jmargail <julien.margail@gmail.com>
2016-07-22 17:01:47 +0200

    fix measure clear icon (require ck-font master5x)

--- src/map/action/measure/Clear.js
+++ src/map/action/measure/Clear.js
@@ -1,19 +1,19 @@
 /**
- * 
+ *
  */
 Ext.define('Ck.map.action.measure.Clear', {
 	extend: 'Ck.map.action.Measure',
 	alias: 'widget.ckmapMeasureClear',
-	
+
 	itemId: 'measureclear',
 	text: '',
-	iconCls: 'fa fa-eraser',
+	iconCls: 'ckfont ck-eraser',
 	tooltip: 'Clear all measures',
-	
+
 	toggleGroup: null,
 	enableToggle: false,
 	type: null,
-	
+
 	doAction: function() {
 		this.clearAll();
 	}
