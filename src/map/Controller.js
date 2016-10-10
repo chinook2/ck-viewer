31c5e2d62f5642789cb0d249e0f09fa494d46b4b
jmargail <julien.margail@gmail.com>
2016-10-10 17:37:36 +0200

    new: OWS Layer extension "topLeftCorner:[x,y]" for WMTS layer if different from bbox (layer or context)

--- src/map/Controller.js
+++ src/map/Controller.js
@@ -418,7 +418,7 @@ Ext.define('Ck.map.Controller', {
 					params = mainOperation.getParams();
 
 					// get grid origin from layer extent or context extent
-					var origin = ol.extent.getTopLeft(layer.getExtent() || owc.getExtent());
+					var origin = layer.getExtension('topLeftCorner') || ol.extent.getTopLeft(layer.getExtent() || owc.getExtent());
 
 					// get resolution from main view. need inverse order
 					var resolutions = owc.getResolutions(false);
