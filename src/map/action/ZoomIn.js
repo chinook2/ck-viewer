a61eee83834e8b4c303e5255d7c9215a4d5b9133
jmargail <julien.margail@gmail.com>
2016-12-15 17:18:01 +0100

    add ckmapZoomto - zoom to specified extent fix Ck.getMap to this.getMap

--- src/map/action/ZoomIn.js
+++ src/map/action/ZoomIn.js
@@ -2,7 +2,7 @@
  * Basic action to zoom in the map (zoom level + 1).
  *
  * Use on a {@link Ext.button.Button} in a {@link Ext.toolbar.Toolbar}.
- * 
+ *
  *		{
  *			xtype: "button",
  *			scale: "large",
@@ -15,17 +15,17 @@
 Ext.define('Ck.map.action.ZoomIn', {
 	extend: 'Ck.Action',
 	alias: "widget.ckmapZoomin",
-	
+
 	itemId: 'zoomin',
 	text: '',
 	iconCls: 'fa fa-search-plus',
 	tooltip: 'Zoom in',
-	
+
 	/**
 	 * Zoom in the map on click.
 	 */
 	doAction: function(btn) {
-		var map = Ck.getMap();
+		var map = this.getMap();
 		map.setZoom( map.getZoom() + 1 );
 	}
 });
--- src/map/action/ZoomOut.js
+++ src/map/action/ZoomOut.js
@@ -2,7 +2,7 @@
  * Basic action to zoom out the map (zoom level - 1).
  *
  * Use on a {@link Ext.button.Button} in a {@link Ext.toolbar.Toolbar}.
- * 
+ *
  *		{
  *			xtype: "button",
  *			scale: "large",
@@ -15,17 +15,17 @@
 Ext.define('Ck.map.action.ZoomOut', {
 	extend: 'Ck.Action',
 	alias: "widget.ckmapZoomout",
-	
+
 	itemId: 'zoomout',
 	text: '',
 	iconCls: 'fa fa-search-minus',
 	tooltip: 'Zoom out',
-	
+
 	/**
 	 * Zoom out the map on click.
 	 */
 	doAction: function(btn) {
-		var map = Ck.getMap();
+		var map = this.getMap();
 		map.setZoom( map.getZoom() - 1 );
 	}
 });
