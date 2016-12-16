5c8345f527c6c9a6ba09487d19c5206815d369a3
jmargail <julien.margail@gmail.com>
2016-12-16 16:22:06 +0100

    fix context getResolutions work on clone array (prevent reverse resolution in ol.tilegrid.WMTS)

--- src/format/OWSContext.js
+++ src/format/OWSContext.js
@@ -1,9 +1,9 @@
 /**
- * 
+ *
  */
 Ext.define('Ck.format.OWSContext', {
 	alternateClassName: ['Ck.owc', 'Ck.Owc'],
-	
+
 	config: {
 		extent		: null,
 		projection	: null,
@@ -11,49 +11,49 @@ Ext.define('Ck.format.OWSContext', {
 		resolutions	: null,
 		layers		: []
 	},
-	
+
 	requires: [
 		'Ck.format.OWSContextLayer'
 	],
-	
+
 	/**
 	 * Constructor
 	 * @params {Object}
 	 */
 	constructor: function(config) {
 		var scales, proj, extent, resolutions = [], data = config.data;
-		
+
 		// Scales
 		scales = data.properties.scales;
-		
+
 		// Projection
 		proj = ol.proj.get(data.properties.srs || Ck.defaults.srs);
-		
+
 		// Extent
 		if(!data.properties.bbox) {
 			extent = proj.getWorldExtent() || Ck.defaults.extent;
 		} else {
 			extent = data.properties.bbox;
 		}
-		
+
 		// Resolutions
 		if(!Ext.isEmpty(scales) && Ext.isArray(scales)) {
 			scales.forEach(function(o) {
 				if(o &&  o.res) resolutions.push(o.res);
 			});
 		}
-		
+
 		Ext.apply(config, {
 			projection	: proj,
 			extent		: extent,
 			scales		: scales,
 			resolutions	: resolutions
 		});
-		
+
 		this.initConfig(config);
-		
+
 		var layers = this.getLayers();
-		
+
 		// Layers
 		for(var i = 0; i < data.features.length; i++) {
 			layers.push(new Ck.owsLayer({
@@ -62,17 +62,19 @@ Ext.define('Ck.format.OWSContext', {
 			}));
 		}
 	},
-	
+
 	/**
 	 * Return resolutions in an array.
 	 * @param {Boolean} True to return in ascending order
 	 */
 	getResolutions: function(ascending) {
-		if(!Ext.isEmpty(this._resolutions) && Ext.isArray(this._resolutions) && this._resolutions.length > 1) {
-			if((ascending && this._resolutions[0] > this._resolutions[1]) || (!ascending && this._resolutions[0] < this._resolutions[1])) {
-				this._resolutions.reverse();
-			}	
+		var res = this._resolutions;
+		if(!Ext.isEmpty(res) && Ext.isArray(res) && res.length > 1) {
+			res = res.slice(0); //clone Array (prevent reverse resolution in tilegrid.WMTS)
+			if((ascending && res[0] > res[1]) || (!ascending && res[0] < res[1])) {
+				res.reverse();
+			}
 		}
-		return this._resolutions;
+		return res;
 	}
 });
