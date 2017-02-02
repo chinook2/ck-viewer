e48a438cf1a80804e664176e3353edb07e66ab3e
jmargail <julien.margail@gmail.com>
2017-02-02 14:06:13 +0100

    fix getLegendGraphic if layer have specific STYLES in WMS url

--- src/legend/plugin/LegendGraphic.js
+++ src/legend/plugin/LegendGraphic.js
@@ -10,11 +10,11 @@ Ext.define('Ck.legend.plugin.LegendGraphic', {
 	requires: [
 		'Ext.Img'
 	],
-	
+
 	config: {
 		map: null
 	},
-	
+
 	/**
 	 * @var {Ck.legend.Controller}
 	 */
@@ -48,13 +48,13 @@ Ext.define('Ck.legend.plugin.LegendGraphic', {
 	 */
 	onItemclick: function(tree, record, item, index, e) {
 		var layer = record.get('layer');
-		
+
 		if(!(layer instanceof ol.layer.Group)) {
 			var src = layer.getSource();
 			if(src.getUrl && Ext.isString(src.getUrl()) && e.target.tagName == "SPAN" && record.isLeaf() && layer && layer.ckLayer && layer.ckLayer.getData().properties.legend &&
 				!Ext.String.startsWith(e.target.className.trim(), "x-action") && !Ext.String.startsWith(e.target.className.trim(), "x-tree-checkbox")) {
 				var graphic = record.get('graphic');
-				
+
 				if(graphic && graphic.getEl() && graphic.getEl().dom && Ext.get(graphic.getEl().dom.id)) {
 					if(graphic.success) {
 						graphic.setVisible(graphic.hidden);
@@ -64,7 +64,7 @@ Ext.define('Ck.legend.plugin.LegendGraphic', {
 					tr.classList.add("ck-layer-legendgraphic");
 					var td = tr.insertCell();
 					td.colSpan = 2;
-					
+
 					var imgSrc = this.generateSrc(layer);
 					if (imgSrc) {
 						graphic = Ck.create("Ext.Img", {
@@ -91,7 +91,7 @@ Ext.define('Ck.legend.plugin.LegendGraphic', {
 
 	/**
 	 * On resolution change, update image because legend can be differente
-	 * 
+	 *
 	 */
 	updateSrc: function(rcd, evt) {
 		var img = rcd.get("graphic");
@@ -109,9 +109,9 @@ Ext.define('Ck.legend.plugin.LegendGraphic', {
 	generateSrc: function(lyr) {
 		var src = lyr.getSource();
 		if (!src.getUrl) return false;
-		
+
 		var url = lyr.ckLayer.getData().properties.legend;
-		
+
 		if(Ext.isString(url)) {
 			// URL variables
 			var urlVar = {
@@ -120,21 +120,21 @@ Ext.define('Ck.legend.plugin.LegendGraphic', {
 			if(Ext.manifest.ckClient) {
 				Ext.applyIf(urlVar, Ext.manifest.ckClient);
 			};
-			
+
 			// Apply variables
 			var urlTpl = new Ext.Template(url);
 			url = urlTpl.applyTemplate(urlVar);
 		} else {
 			url = "";
 		}
-		
+
 		if(url.indexOf("http") !== 0) {
-			url = src.getUrl() + "?SERVICE=WMS&REQUEST=GetLegendGraphic&FORMAT=image/png&LAYER=" + src.getParams().LAYERS + "&TRANSPARENT=true" + url;
+			url = src.getUrl() + "?SERVICE=WMS&REQUEST=GetLegendGraphic&FORMAT=image/png&LAYER=" + src.getParams().LAYERS + "&STYLE="+ (src.getParams().STYLES || '') +"&TRANSPARENT=true" + url;
 		}
 
 		return url;
 	},
-	
+
 	/**
 	 * The Ext doc is wrong for the list params !!
 	 * After drag&drop the legend reference is wrong, need to rebuild
@@ -142,7 +142,7 @@ Ext.define('Ck.legend.plugin.LegendGraphic', {
 	onItemremove: function(root, record) {
 		record.set('graphic', false);
 	},
-	
+
 	/**
 	 * Hide img on fail
 	 */
@@ -150,7 +150,7 @@ Ext.define('Ck.legend.plugin.LegendGraphic', {
 		this.graphic.success = false;
 		this.graphic.setVisible(false);
 	},
-	
+
 	/**
 	 * Display img on fail
 	 */
