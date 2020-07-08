708c0c13736ca3f482a5e1ade7a3f6c10bda06aa
FlorentRICHARD44 <florentrichard@hotmail.fr>
2020-07-08 15:08:58 +0200

    FRI - Add action to open a legend Image

---
+++ src/map/action/OpenLegendImage.js
@@ -0,0 +1,114 @@
+/**
+ *
+ */
+Ext.define('Ck.map.action.OpenLegendImage', {
+	extend: 'Ck.Action',
+	alias: "widget.ckOpenLegendImg",
+
+	iconCls: 'fa fa-list-ul',
+	tooltip: 'Open Legend',
+	
+	toggleGroup: 'ckLegendImage',
+
+	requires: [
+		'Ck.Window'
+	],
+	firstView: true,
+	config: {
+		/**
+		 * Width of window
+		 * @prop {Integer}
+		 */
+		winWidth: 250,
+
+		/**
+		 * Height of window
+		 * @prop {Integer}
+		 */
+		winHeight: 400,
+
+		/**
+		 * Title of window
+		 * @prop {Integer}
+		 */
+		winTitle: 'Legend',
+		imgSrc: null,
+		imgPackage: null,
+		imgHeight: null,
+		imgWidth: null
+	},
+
+	/**
+	 * Create and display a windows with print form
+	 */
+	toggleAction: function(btn, pressed) {
+		this.button = btn || {};
+
+		if(!this.win) {
+			this.win = Ext.create(this.classWindow, {
+				title: this.getWinTitle(),
+				height: this.getWinHeight(),
+				scrollable: 'y',
+				resizable: false,
+				modal: false,
+				layout: 'vbox',
+				closeAction: 'hide',
+				parentMap: this.getMap(),
+				items: [{
+					xtype:"container",
+					layout:'fit',
+					scrollable: 'y',
+					bodyPadding: 20,
+					items: [{
+						xtype: 'image',
+						src:Ck.getPath(this.getImgPackage()) + this.getImgSrc(),
+						width: this.getImgWidth(),
+						height: this.getImgHeight()
+					}]
+				}],
+				listeners: {
+					close: function() {
+						this.close();
+					},
+					scope: this
+				}
+			});
+		}
+
+		if(pressed) {
+			this.win.show();
+			if(this.firstView) {
+				this.win.alignTo(this.getMap().getOlMap().getViewport(), "tr", [-40 -this.getWinWidth(), 40]);
+				this.firstView = false;
+			}
+		} else {
+			this.win.hide();
+		}
+
+		// Auto close overview popup when CkView or CkMap is hidden or destroy
+		this.getCkView().getView().on({
+			hide: function () {
+				this.close();
+			},
+			destroy: function () {
+				this.close(true);
+			},
+			scope: this
+		});
+
+		this.getMap().getView().on({
+			hide: function () {
+				this.close();
+			},
+			destroy: function () {
+				this.close(true);
+			},
+			scope: this
+		});
+	},
+
+	close: function(isDestroying) {
+		this.win.hide();
+		if(isDestroying!==true) this.button.setPressed(false);
+	}
+});
