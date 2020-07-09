aa4344219cfda9363bd9f26c953f9f7884a6bf64
FlorentRICHARD44 <florentrichard@hotmail.fr>
2020-07-09 12:07:15 +0200

    FRI - Fix issues with measure tools, due to new OpenLayers version

--- src/Measure.js
+++ src/Measure.js
@@ -360,7 +360,11 @@ Ext.define('Ck.Measure', {
 			} else {
 				var coordinates = geom.getLinearRing(0).getCoordinates();
 				//area = Math.abs(this.wgs84Sphere.geodesicArea(coordinates));
-				area = Math.abs(ol.sphere.getArea(coordinates));
+				if (sourceProj.getUnits() == "m") {
+					area = Math.abs(ol.sphere.getArea(polygon.clone(), {projection:sourceProj}));
+				} else {
+					area = Math.abs(ol.sphere.getArea(polygon.clone().transform(sourceProj, 'EPSG:3857')));
+				}
 			}
 		} else {
 			area = polygon.getArea();
