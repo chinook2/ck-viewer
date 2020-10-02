66032aef2675bd61889143cea1a4cdbb171cc677
Julien Margail <j.margail@geofit.fr>
2020-10-02 07:22:36 +0000

    fix viewmodel 'version'

--- src/view/Model.js
+++ src/view/Model.js
@@ -7,7 +7,8 @@ Ext.define('Ck.view.Model', {
 	alias: 'viewmodel.ckview',
 
 	data: {
-		name: Ext.manifest.name
+		name: Ext.manifest.name,
+		version: Ck.getOption('version')
 	}
 
 	//TODO - add data, formulas and/or methods to support your view
