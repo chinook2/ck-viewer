27406853a7cf3100afbd4517cea7f7e325582ee0
Adrien Gatineau <a.gatineau@geofit.fr>
2021-02-01 09:47:31 +0000

    update click event on mouse position

--- src/map/action/MousePosition.js
+++ src/map/action/MousePosition.js
@@ -34,12 +34,22 @@ Ext.define('Ck.map.action.MousePosition', {
 			},
 			className: 'ol-control ck-mouse-position',
 			//projection: 'EPSG:4326',
-			target: document.getElementById('lblCoord'),
+			target: document.getElementById('lblCoordHide'),
 			//undefinedHTML: '&nbsp;'
 		});
 
 		// TODO : manage in Actions.js if btn is pressed and call toggleAction...
-		if(config.pressed) this.olMap.addControl(this.mousePositionControl);
+		if(config.pressed) this.olMap.addControl(this.mousePositionControl);	
+	},
+
+	/**
+	 * Set label coordinatee
+	 */
+	toggleMousePosition(event){
+		var coordinate = ol.coordinate.toStringXY(event.coordinate,3);
+		if(Ext.ComponentQuery.query('#lblCoord')){
+			Ext.ComponentQuery.query('#lblCoord')[0].setHtml(coordinate);
+		};
 	},
 
 	/**
@@ -49,8 +59,10 @@ Ext.define('Ck.map.action.MousePosition', {
 		if(!this.olMap) return;
 		if(pressed) {
 			this.olMap.addControl(this.mousePositionControl);
+			this.olMap.on('click', this.toggleMousePosition);
 		}else{
 			this.olMap.removeControl(this.mousePositionControl);
+			this.olMap.un("click", this.toggleMousePosition);
 			Ext.ComponentQuery.query('#lblCoord')[0].setHtml('');
 		}
 	}
