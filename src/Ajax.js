e0019805c70b002238d9d55dbf90a4e536abc121
Florent RICHARD <florentrichard@hotmail.fr>
2024-03-15 15:21:49 +0100

    FRI - CK.Ajax - Add try/catch when adding item to localstorage

--- src/Ajax.js
+++ src/Ajax.js
@@ -2,234 +2,237 @@
  *
  */
 Ext.define('Ck.Ajax', {
-	extend: 'Ext.data.Connection',
-	alternateClassName: 'Cks',
-	singleton: true,
+    extend: 'Ext.data.Connection',
+    alternateClassName: 'Cks',
+    singleton: true,
 
-	requires: [
-		'Ck'
-	],
+    requires: [
+        'Ck'
+    ],
 
-	/**
-	 * @ignore
-	 */
-	constructor: function() {
-		this.callParent(arguments);
+    /**
+     * @ignore
+     */
+    constructor: function () {
+        this.callParent(arguments);
 
-		this.ls = new Ext.util.LocalStorage({
-			id: 'Ck-' + Ext.manifest.name
-		});
+        this.ls = new Ext.util.LocalStorage({
+            id: 'Ck-' + Ext.manifest.name
+        });
 
-		// Global disable ajax cache and ajax global events
-		// By default disabled.
-		if (Ck.getOption('ajaxCache') === true) {
-			Ext.Ajax.on({
-				beforerequest: this.onBeforeRequest,
-				requestcomplete: this.onRequestComplete,
-				requestexception: this.onRequestException,
-				scope: this
-			});
-		}
+        // Global disable ajax cache and ajax global events
+        // By default disabled.
+        if (Ck.getOption('ajaxCache') === true) {
+            Ext.Ajax.on({
+                beforerequest: this.onBeforeRequest,
+                requestcomplete: this.onRequestComplete,
+                requestexception: this.onRequestException,
+                scope: this
+            });
+        }
 
 
-		// Ext.Ajax.setDefaultPostHeader('application/json; charset=UTF-8');
-		// if (Ck.getOption('defaultPostHeader')) {
-			// Ext.Ajax.setDefaultPostHeader(Ck.getOption('defaultPostHeader'));
-		// }
-	},
+        // Ext.Ajax.setDefaultPostHeader('application/json; charset=UTF-8');
+        // if (Ck.getOption('defaultPostHeader')) {
+        // Ext.Ajax.setDefaultPostHeader(Ck.getOption('defaultPostHeader'));
+        // }
+    },
 
-	get: function(options) {
-		options.method = 'GET';
+    get: function (options) {
+        options.method = 'GET';
 
-		// Prod caching with unique build timestamp param (force reload for new build only)
-		if(Ck.getEnvironment() == 'production'){
-			if(Ext.manifest.loader && Ext.manifest.loader.cache){
-				options.url = Ext.urlAppend(options.url, Ext.Ajax.getDisableCachingParam() + '=' + Ext.manifest.loader.cache);
-			}
-		}
+        // Prod caching with unique build timestamp param (force reload for new build only)
+        if (Ck.getEnvironment() == 'production') {
+            if (Ext.manifest.loader && Ext.manifest.loader.cache) {
+                options.url = Ext.urlAppend(options.url, Ext.Ajax.getDisableCachingParam() + '=' + Ext.manifest.loader.cache);
+            }
+        }
 
-		this.request(options);
-	},
+        this.request(options);
+    },
 
-	/**
-	 * Perform a POST request
-	 * @param {Object}
-	 * @todo Chek if we have file to upload. Use this if yes : application/x-www-form-urlencoded;charset=UTF-8
-	 */
-	post: function(options) {
-		Ext.applyIf(options, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json; charset=UTF-8'
-			}
-		});
+    /**
+     * Perform a POST request
+     * @param {Object}
+     * @todo Chek if we have file to upload. Use this if yes : application/x-www-form-urlencoded;charset=UTF-8
+     */
+    post: function (options) {
+        Ext.applyIf(options, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+        });
 
-		if(options.encode !== false && options.params) {
-			options.params = Ext.encode(options.params);
-			delete options.encode;
-		}
+        if (options.encode !== false && options.params) {
+            options.params = Ext.encode(options.params);
+            delete options.encode;
+        }
 
-		this.request(options);
-	},
+        this.request(options);
+    },
 
-	put: function(options) {
-		Ext.applyIf(options, {
-			method: 'PUT',
-			headers: {
-				'Content-Type': 'application/json; charset=UTF-8'
-			}
-		});
+    put: function (options) {
+        Ext.applyIf(options, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+        });
 
-		if(options.encode !== false && options.params) {
-			options.params = Ext.encode(options.params);
-			delete options.encode;
-		}
+        if (options.encode !== false && options.params) {
+            options.params = Ext.encode(options.params);
+            delete options.encode;
+        }
 
-		this.request(options);
-	},
+        this.request(options);
+    },
 
-	del: function(options) {
-		Ext.applyIf(options, {
-			method: 'DELETE',
-			headers: {
-				'Content-Type': 'application/json; charset=UTF-8'
-			}
-		});
+    del: function (options) {
+        Ext.applyIf(options, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+        });
 
-		if(options.encode !== false && options.params) {
-			options.params = Ext.encode(options.params);
-			delete options.encode;
-		}
+        if (options.encode !== false && options.params) {
+            options.params = Ext.encode(options.params);
+            delete options.encode;
+        }
 
-		this.request(options);
-	},
+        this.request(options);
+    },
 
-	request: function(options) {
-		options.disableCaching = false;
-		//<debug>
-		// Dev Mode use standard disable cache system : each call is unique
-		options.disableCaching = true;
-		//</debug>
+    request: function (options) {
+        options.disableCaching = false;
+        //<debug>
+        // Dev Mode use standard disable cache system : each call is unique
+        options.disableCaching = true;
+        //</debug>
 
-		this.callParent(arguments);
-	},
+        this.callParent(arguments);
+    },
 
-	/**
-	 * Perform an AJAX request using XHR Level 2.
-	 * @param {Object}
-	 */
-	xhr: function(options) {
-		Ext.applyIf(options, {
-			method	: "POST",
-			url		: "index.php",
-			params	: {},
-			files	: [],
-			scope	: this,
-			success	: Ext.emptyFn,
-			failure	: Ext.emptyFn
-		});
+    /**
+     * Perform an AJAX request using XHR Level 2.
+     * @param {Object}
+     */
+    xhr: function (options) {
+        Ext.applyIf(options, {
+            method: "POST",
+            url: "index.php",
+            params: {},
+            files: [],
+            scope: this,
+            success: Ext.emptyFn,
+            failure: Ext.emptyFn
+        });
 
-		var xhr = new XMLHttpRequest();
-		var _async = (options.async !== false) ? true : false;
+        var xhr = new XMLHttpRequest();
+        var _async = (options.async !== false) ? true : false;
 
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == 4) {
-				if(xhr.status == 200 || xhr.status == 0) {
-					options.success.call(options.scope, xhr);
-				} else {
-					options.failure.call(options.scope, xhr);
-				}
-			}
-		};
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200 || xhr.status == 0) {
+                    options.success.call(options.scope, xhr);
+                } else {
+                    options.failure.call(options.scope, xhr);
+                }
+            }
+        };
 
-		var fData;
-		if(Ext.isObject(options.params)) {
-			fData = new FormData();
-			for(var key in options.params) {
-				fData.append(key, options.params[key]);
-			}
+        var fData;
+        if (Ext.isObject(options.params)) {
+            fData = new FormData();
+            for (var key in options.params) {
+                fData.append(key, options.params[key]);
+            }
 
-			var f;
-			for(var i in options.files) {
-				f = options.files[i];
-				fData.append(f.name, f.value, f.filename);
-			}
-		} else {
-			fData = options.params;
-		}
+            var f;
+            for (var i in options.files) {
+                f = options.files[i];
+                fData.append(f.name, f.value, f.filename);
+            }
+        } else {
+            fData = options.params;
+        }
 
-		xhr.open(options.method, options.url, _async);
+        xhr.open(options.method, options.url, _async);
 
-		if(Ext.isObject(options.headers)) {
-			for(var h in options.headers) {
-				var v = options.headers[h];
-				xhr.setRequestHeader(h, v);
-			}
-		}
-		xhr.send(fData);
-	},
+        if (Ext.isObject(options.headers)) {
+            for (var h in options.headers) {
+                var v = options.headers[h];
+                xhr.setRequestHeader(h, v);
+            }
+        }
+        xhr.send(fData);
+    },
 
-	isCacheAvailable: function(options) {
-		// Only cache GET request
-		if(options.method != 'GET') {
-			return false;
-		}
+    isCacheAvailable: function (options) {
+        // Only cache GET request
+        if (options.method != 'GET') {
+            return false;
+        }
 
-		// Disable Ajax cache for a specific call
-		if(options.nocache===true) {
-			return false;
-		}
+        // Disable Ajax cache for a specific call
+        if (options.nocache === true) {
+            return false;
+        }
 
-		// Disable Ajax cache by URL param (in production)
-		if(Ck.params.hasOwnProperty('nocache')) {
-			return false;
-		}
+        // Disable Ajax cache by URL param (in production)
+        if (Ck.params.hasOwnProperty('nocache')) {
+            return false;
+        }
 
-		// Disable Ajax cache (in testing and development). Allow 'forcecache' for testing and development.
-		if(Ck.getEnvironment() != 'production' && !Ck.params.hasOwnProperty('forcecache')) {
-			return false;
-		}
+        // Disable Ajax cache (in testing and development). Allow 'forcecache' for testing and development.
+        if (Ck.getEnvironment() != 'production' && !Ck.params.hasOwnProperty('forcecache')) {
+            return false;
+        }
 
-		return true;
-	},
+        return true;
+    },
 
-	onBeforeRequest: function(conn, options, eOpts) {
-		if(!this.isCacheAvailable(options)) return true;
-		// TODO : test cache validity
+    onBeforeRequest: function (conn, options, eOpts) {
+        if (!this.isCacheAvailable(options)) return true;
+        // TODO : test cache validity
 
-		var res = this.ls.getItem(options.url);
-		if(res) {
-			//<debug>
-			Ck.Notify.info('Request from Cache : '+ options.url);
-			//</debug>
+        var res = this.ls.getItem(options.url);
+        if (res) {
+            //<debug>
+            Ck.Notify.info('Request from Cache : ' + options.url);
+            //</debug>
 
-			var response = {
-				responseText: res
-			}
+            var response = {
+                responseText: res
+            }
 
-			Ext.callback(options.success, options.scope, [response, options]);
-			return false;
-		}
-	},
+            Ext.callback(options.success, options.scope, [response, options]);
+            return false;
+        }
+    },
 
-	onRequestComplete: function(conn) {
-		var options = conn.options;
-		var response = conn.result;
+    onRequestComplete: function (conn) {
+        var options = conn.options;
+        var response = conn.result;
 
-		//<debug>
-		//Ck.Notify.info('Request success : '+ options.url);
-		//</debug>
+        //<debug>
+        //Ck.Notify.info('Request success : '+ options.url);
+        //</debug>
 
-		if(!this.isCacheAvailable(options)) return true;
+        if (!this.isCacheAvailable(options)) return true;
+        try {
+            this.ls.setItem(options.url, response.responseText);
+        } catch (e) {
+            console.log('Ck.Ajax::onRequestComplete - Localstorage probably full : ' + e.message);
+        }
+    },
 
-		this.ls.setItem(options.url, response.responseText);
-	},
+    onRequestException: function (conn) {
+        var options = conn.options;
+        var response = conn.result;
 
-	onRequestException: function(conn) {
-		var options = conn.options;
-		var response = conn.result;
-
-		Ck.Notify.error('Request failure : ' + options.url, response);
-		// TODO : parse error message ...
-	}
+        Ck.Notify.error('Request failure : ' + options.url, response);
+        // TODO : parse error message ...
+    }
 });
